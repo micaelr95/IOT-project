e0da91b3615f2b7b03282ba73716b9c3de2aa641
Micael Rodrigues <micaelr95@outlook.pt>
2019-01-01 23:31:52 +0000

    LDR sesor working

--- index.py
+++ index.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import time
+
 app = Flask(__name__)
 
+LDR_PIN = 17
+SM_PIN = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(LDR_PIN, GPIO.OUT)
+GPIO.output(LDR_PIN, GPIO.LOW)
+
 @app.route("/")
-def index(ldr=None):
-    return render_template("index.html", ldr=ldr)
+def index():
+    GPIO.setup(LDR_PIN, GPIO.IN)
+    templateData = {
+        'ldr': GPIO.input(LDR_PIN)
+    }
+    return render_template("index.html", **templateData)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host= '0.0.0.0', debug=True)
