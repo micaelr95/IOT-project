ce27b1c4ac485d7ec44cc9c3356422a17b760f44
Micael Rodrigues <micaelr95@outlook.pt>
2019-01-01 23:57:26 +0000

    LDR sensor and Servo motor working

--- index.py
+++ index.py
@@ -6,19 +6,39 @@ app = Flask(__name__)
 
 LDR_PIN = 17
 SM_PIN = 18
+sm_state = 7.5
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
 GPIO.setup(LDR_PIN, GPIO.OUT)
+GPIO.setup(SM_PIN, GPIO.OUT)
 GPIO.output(LDR_PIN, GPIO.LOW)
+GPIO.setup(LDR_PIN, GPIO.IN)
+pwm=GPIO.PWM(SM_PIN, 50)
+pwm.start(sm_state)
 
 @app.route("/")
 def index():
-    GPIO.setup(LDR_PIN, GPIO.IN)
+    ldr_state = GPIO.input(LDR_PIN)
+    if ldr_state==0:
+        sm_state = 7.5
+    else:
+        sm_state = 12.5
+
+    pwm.ChangeDutyCycle(sm_state)
     templateData = {
-        'ldr': GPIO.input(LDR_PIN)
+        'ldr': ldr_state,
+        'sm': sm_state
     }
     return render_template("index.html", **templateData)
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True)
+    try:
+        app.run(host= '0.0.0.0', debug=True)
+    except KeyboardInterrupt:
+        print('Key Interrupt')
+    finally:
+        print("STOP")
+        pwm.stop()
+        GPIO.cleanup()
+    
