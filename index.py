272c79bf99e840c315667a7e2864d461b4d7b967
Micael Rodrigues <micaelr95@outlook.pt>
2019-01-06 17:50:04 +0000

    Code documentation

--- index.py
+++ index.py
@@ -4,12 +4,16 @@ import time
 
 app = Flask(__name__)
 
+# Guarda os pinos onde estao conetados os sensores e os atuadores
 LDR_PIN = 17
 SM_PIN = 18
+
+# Guarda o estado do atuador
 sm_state = 7.5
 
+# Iniciação do GPIO
 GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
+#GPIO.setwarnings(False)
 GPIO.setup(LDR_PIN, GPIO.OUT)
 GPIO.setup(SM_PIN, GPIO.OUT)
 GPIO.output(LDR_PIN, GPIO.LOW)
@@ -19,13 +23,17 @@ pwm.start(sm_state)
 
 @app.route("/")
 def index():
+    '''Função principal que retorna o index'''
     ldr_state = GPIO.input(LDR_PIN)
+    # Se não houver luz fecha o servomotor senao abre
     if ldr_state==0:
         sm_state = 7.5
     else:
         sm_state = 12.5
 
+    # Muda o valor do servomotor
     pwm.ChangeDutyCycle(sm_state)
+    # Dicionario que guarda as variaveis para a página
     templateData = {
         'ldr': ldr_state,
         'sm': sm_state
@@ -34,9 +42,12 @@ def index():
 
 @app.route("/<devicename>/<action>")
 def control(devicename, action):
+    '''Função para controlar os atuadores. Recebe o nome do atuador e o valor'''
     if devicename == "sm":
         sm_state = (float(action))
+        # Muda o valor do servomotor
         pwm.ChangeDutyCycle(sm_state)
+    # Dicionario que guarda as variaveis para a página
     templateData = {
         'ldr': GPIO.input(LDR_PIN),
         'sm': sm_state
